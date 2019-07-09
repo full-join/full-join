803c6952c79dedbd177aade8ae8a91cc3a1d9dac
Julie Nisbet <jmnisbet@gmail.com>
2019-07-09 16:49:02 -0700

    whitespace and typos

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature "Home Page", type: :feature do
-	scenario "vistiing the home pace" do
-		visit root_path
-		expect(page).to have_content "Welcome to Full Join"
-	end
+  scenario "visting the home page" do
+    visit root_path
+    expect(page).to have_content "Welcome to Full Join"
+  end
 end
