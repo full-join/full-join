8de99c19c6cf8a0cf23c07e38f1790d0a31fe02a
tiffanyisapr0 <tiffany@procore.com>
2019-07-18 13:49:21 -0500

    refactored home to pages

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Home Page", type: :feature do
-  scenario "visting the home page" do
+  scenario "visting the pages page" do
     visit root_path
     expect(page).to have_content "Welcome to Full Join"
   end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "home page", type: :request do
+RSpec.describe "pages page", type: :request do
   describe "GET /" do
-    it "renders the home page" do
+    it "renders the pages page" do
       get root_path
       expect(response).to have_http_status(200)
     end
