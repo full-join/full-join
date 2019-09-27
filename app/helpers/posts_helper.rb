d16ec41a32703ffe9af9f3f4caddeb309acd0a0c
Julie Nisbet <jmnisbet@gmail.com>
2019-09-26 18:29:54 -0700

    General clean up
    * Added a new button to the nav bar
    * Added a bunch of styles
    * Added a spec for post deleting

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def generate_class(index)
+    case index % 6
+    when 0
+      return 'is-primary'
+    when 1
+      return 'is-link'
+    when 2
+      return 'is-info'
+    when 3
+      return 'is-success'
+    when 4
+      return 'is-warning'
+    when 5 
+      return 'is-danger'
+    end
+  end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "posts", type: :request do
-  
-
   describe "creating a post" do
     it "creates a post" do
       params = {post:{title:"My Title",text:"My text"}}
       expect{post posts_path, params: params}.to change{Post.count}.by(1)
-      
+    end
+  end
+
+  describe "deleting a post" do
+    context "without errors" do
+      let(:post) {Post.create(title: "my title", text: "my text")}
+      it "deletes a post" do
+        post
+        expect{delete post_path(post)}.to change{Post.count}.by(-1)
+      end
     end
   end
 end
