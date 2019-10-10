54b7d637bcb4312fc5eebda02a995d06744cdc53
Ann-Marie Harper <annmarie.grace.harper@gmail.com>
2019-10-10 17:25:22 -0500

    Added Users Controller Controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'profile', to: 'users#show'
 end
