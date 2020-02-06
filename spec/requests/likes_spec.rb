a9ef42c0b75045d01d4fbb5be9e613ca53c55c5a
Julie Nisbet <jmnisbet@gmail.com>
2020-02-06 11:54:29 -0800

    adding likes
    updating request spec and controller to create likes

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "likes request", type: :request do
   let (:user){FactoryBot.create(:user)}
-  let (:post){FactoryBot.create(:post)}
+  let (:test_post){FactoryBot.create(:post)}
   describe "#create" do
     it "should create a like" do
-      expect {post user_likes_path(user)}.to change{Like.count}.by(1)
+      expect{post user_likes_path(user), params: {
+        like: {
+          user_id: user.id,
+          post_id: test_post.id
+        }
+      }}.to change{Like.count}.by(1)
     end
   end
-  
 end
