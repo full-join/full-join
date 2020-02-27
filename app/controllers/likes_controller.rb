13809b3c37ae9dc62eef96009f7defc6d3c4d334
Ann-Marie Harper <annmarie.grace.harper@gmail.com>
2020-02-27 13:42:24 -0600

    finishing refactor

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,6 +6,6 @@ class LikesController < ApplicationController
 
   private
   def like_params
-    params.require(:like).permit(:post_id, :user_id)
+    params.require(:like).permit(:blog_id, :user_id)
   end
 end
